96ad73c08fc4007f22452bc3efa3536d5e2ad59e
Limax <limax10000100100000@gmail.com>
2020-03-14 00:13:23 +0100

    Classe pels personatges

---
+++ BaboGame_test_2/Character.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+
+public class Character
+{
+    private Texture2D _CharTexture;
+    public Vector2D _CharPosition;
+
+	public Character(Texture2D _texture)
+	{
+        _CharTexture = _texture;
+	}
+
+    public void Update()
+    {
+
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+
+    }
+
+
+}
